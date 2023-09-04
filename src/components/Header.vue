49974be0b49c13c13ebd36b9f37c2226dc808606
Archie888-A8Group <84933486+Archie888-A8Group@users.noreply.github.com>
2023-09-05 00:49:31 +0300

    Create Header.vue

---
+++ src/components/Header.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="header">
+    <button @click="toggleHeader">Toggle Header</button>
+    <input v-model="searchText" @keyup.enter="performSearch" type="text" placeholder="Search">
+    <button @click="performSearch">Search</button>
+    <button @click="showOptions">Options</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      searchText: '',
+      isHidden: false,
+    };
+  },
+  methods: {
+    toggleHeader() {
+      this.isHidden = !this.isHidden;
+    },
+    performSearch() {
+      // Implement your search logic here
+      // You can emit an event to notify the parent component about the search.
+      this.$emit('search', this.searchText);
+    },
+    showOptions() {
+      // Implement options functionality
+    },
+  },
+};
+</script>
+
+<style scoped>
+/* Add your header styling here */
+.header {
+  background-color: #333;
+  color: white;
+  padding: 10px;
+  display: flex;
+  justify-content: space-between;
+}
+</style>
